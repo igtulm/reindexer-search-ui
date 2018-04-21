a21e8d5a064aa23fb974b6191dccb1a12ccc6e08
Igor Tulmentyev <igtulm@gmail.com>
2018-04-21 11:54:44 +0400

    improve state.items line

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -10,12 +10,7 @@ export default {
   [types.GET_ENTITIES.SUCCESS](state, payload) {
     const { items, total, isGreedy } = payload;
 
-    if (isGreedy) {
-      state.items = [...state.items, ...items];
-    } else {
-      state.items = items;
-    }
-
+    state.items = isGreedy ? [...state.items, ...items] : items;
     state.total = total;
     state.isLoading = false;
   },
